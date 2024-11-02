af034cbc95e8b72ee6a08595bf8d36c178c7c1df
Israel <almanzaisrael75@gmail.com>
2024-11-01 23:51:32 -0400

    creando nuevos modelos

--- src/migrations/20241101210301-create-user.js
+++ src/migrations/20241101210301-create-user.js
@@ -18,14 +18,14 @@ module.exports = {
       password: {
         type: Sequelize.STRING
       },
-      /* createdAt: {
+      createdAt: {
         allowNull: false,
         type: Sequelize.DATE
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
-      } */
+      }
     });
   },
   async down(queryInterface, Sequelize) {
